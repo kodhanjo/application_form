2621478e903927c775e23c794101461d89b3f56b
kodhanjo <felix.osambo@student.moringaschool.com>
2021-06-14 09:11:25 +0300

    added onclick scripts on submit button

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ $(document).ready(function() {
     }
   );
   });
+  // ===== submit button onclick====
+  // Onclick
+
+(function () {
+  "use strict";
+  
+  var flip = document.getElementById('flip'),
+      button = document.getElementById('btn1');
+
+  button.onclick = function () {
+      flip.className = 'animate';
+  };
+
+})();
+
+(function () {
+  "use strict";
+  
+  var container = document.getElementById('container'),
+      button = document.getElementById('btn2');
+
+  button.onclick = function () {
+      container.className = 'opacity';
+  };
+
+})();
+// ====end of onclick button======
